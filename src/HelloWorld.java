f75b991ae6f9d5200d5f49bebc95181d838af4b1
isaacachenbach <isaac.achenbach1@gmail.com>
2023-04-09 18:59:55 -0500

    finished syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,36 @@
 package src;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myFavoriteNumber = 1;
+
+        String myString = "cool";
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int i = 4;
+        i = i + 5;
+        i += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+
+
+
+    }
 }
